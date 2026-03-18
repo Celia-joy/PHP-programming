800e58a6dd66f4b80eb52eb88db1e3645bb90063
Celia <joyihirwecelia@gmail.com>
2026-03-18 08:09:32 +0200

    Code base

--- PHP_Project/delete.php
+++ PHP_Project/delete.php
@@ -1,10 +1,9 @@
 <?php
 include 'connection.php';
 
-// Get the user id from the URL
 $id = $_GET['id'];
 
-// Delete the user
+
 $sql = "DELETE FROM users WHERE id = $id";
 
 if ($conn->query($sql)) {
